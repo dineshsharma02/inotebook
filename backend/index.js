68a117924869413df24edd064d4b280b1df8dacb
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-05 23:21:36 +0530

    added routes of api

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,11 @@ const express = require('express');
 connectToMongo();
 const app = express()
 const port = 3000
-app.get('/',(req,res)=>{
-    res.send("hello there!")
-})
 
 
-app.get('/api/v1/login',(req,res)=>{
-    res.send("hello there!this is a login page!!!")
-})
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 
 app.listen(port,()=>{
     console.log(`Example app listening at http://localhost:${port}`);
