cf77c4b82cf805c45bbf54554ea9b0b7765d9b5d
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-05 23:45:19 +0530

    stored data of user in mongodb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
-router.get('/',(req,res)=>{
-    obj = {
-        a:'this',
-        number:55,
-    }
-    res.json(obj)
+router.post('/',(req,res)=>{
+    console.log(req.body);
+    const user = User(req.body)
+    user.save()
+    res.send("Hello ")
 })
 
 module.exports = router
