d4e9588b4a4e7f8531e516cf11340c68e82de9e5
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-06 23:13:56 +0530

    added auth endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,18 +2,26 @@ const express = require('express');
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
-router.post('/',[
+router.post('/createuser',[
+    //if there are errors return bad request and the errors 
     body('name',"Enter a valid name").isLength({ min: 5 }),
     body('email',"Enter a valid email").isEmail(),
     body('password',"Enter a valid password with length more than 5").isLength({ min: 5 }),
-],(req,res)=>{
+],async (req,res)=>{
+  //returning the array of the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    //checking if the user with email already exists
+    let user = await User.findOne({email:req.body.email});
+    if (user){
+      return res.status(400).json({"error":"the user already exists with this email address."})
+    }
     
 
-    User.create({
+    user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
