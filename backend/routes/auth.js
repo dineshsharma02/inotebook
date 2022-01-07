13082285191006e97382429538e933f517165031
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-07 21:36:47 +0530

    login endpoint created and sent authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,4 +49,43 @@ router.post('/createuser',[
     }
 })
 
+
+
+router.post('/login',[
+  //if there are errors return bad request and the errors 
+  body('email',"Enter a valid email").isEmail(),
+  body('password',"Password cannot be blank").exists(),
+],async (req,res)=>{
+//returning the array of the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {email,password} = req.body;
+  //checking if the user with email already exists
+try{
+  let user = await User.findOne({email:email});
+  if (!user){
+    return res.status(400).json({"error":"Please enter valid credentials"})
+  }
+  const passwordComapare = await bcrypt.compare(password,user.password);
+  if (!passwordComapare){
+    return res.status(400).json({"error":"Please enter valid credentials"})
+  }
+    const data={
+      user:{
+        id:user.id
+      }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET)
+
+    res.json({authtoken})
+  }
+  catch(error){
+    console.error(error.message);
+    res.status(500).send("Internal server occured");
+    
+  }
+})
+
 module.exports = router
