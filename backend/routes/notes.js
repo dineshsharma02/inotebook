47ae8b2b178a5dd4c2ffb6591f6d79f73b926362
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-10 21:56:30 +0530

    endpoint for updating note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -44,4 +44,38 @@ router.post(
   }
 );
 
+//Route3 - updating note :PUT "api/notes/updatenote". Login required
+router.put(
+    "/updatenote/:id",
+    fetchuser,
+    async (req, res) => {
+      //returning the array of the errors
+      try {
+        const { title, description, tag } = req.body;
+        const newNote = {};
+        if(title){
+            newNote.title=title;
+        }
+        if(description){
+            newNote.description=description;
+        }
+        if(tag){
+            newNote.tag=tag
+        }
+        let note = await Note.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not found")
+        }
+        if(note.user.toString()!=req.user.id){
+            return res.status(402).send("Not allowed")
+        }
+        note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+        res.json({note});
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server occured");
+      }
+    }
+  );
+
 module.exports = router;
