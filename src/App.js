f974cf545a88d0aa5c7c16e813cbcf3ba02f6719
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-11 01:09:55 +0530

    basic component structuring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+    <Router>
+      <div>
+      <Navbar/>
+        <Routes>
+          <Route exact path="/about" element={<About/>}></Route>
+          <Route exact path="/" element={<Home/>}></Route>
+        </Routes>
+      </div>
+    </Router>
+      
       <h1>This is iNotebook project</h1>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+            This is home page
+        </div>
+        </div>
+    )
+}
+
+export default Home
